6b58a827ea5373848401dac84d11f608ca1420f5
AsusVivobook <RomanovCopy@gmail.com>
2025-09-28 19:09:29 +0300

    2809251909

---
+++ TimeWebAI/TimeWebAI/Infrastructure/RelayCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TimeWebAI.Infrastructure
+{
+    [Serializable]
+    public class RelayCommand: ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+        private readonly object? _fixedParameter;
+
+        public RelayCommand(Action<object?> execute, Predicate<object?>? canExecute = null,
+            object? fixedParameter = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? (_ => true);
+            _fixedParameter = fixedParameter;
+        }
+
+        public void Execute(object? parameter)
+        {
+            var actualParameter = parameter ?? _fixedParameter;
+            _execute.Invoke(actualParameter);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            var actualParameter = parameter ?? _fixedParameter;
+            return _canExecute.Invoke(actualParameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
---
+++ TimeWebAI/TimeWebAI/Interfaces/IWebViewService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeWebAI.Interfaces
+{
+    public interface IWebViewService
+    {
+        string? AgentId { get; set; }
+        Task LoadWidgetAsync();
+    }
+}
